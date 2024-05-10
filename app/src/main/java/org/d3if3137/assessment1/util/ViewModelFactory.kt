f8abafb5ab8bb5adaab06c6fdf7e2ed9af454f91
Wijdan Ardra Fulvian <wijdan0208@gmail.com>
2024-05-10 19:10:00 +0700

    Menambahkan teks strings dan menambahkan data lainnya

--- app/src/main/java/org/d3if3137/assessment1/util/ViewModelFactory.kt
+++ app/src/main/java/org/d3if3137/assessment1/util/ViewModelFactory.kt
@@ -1,4 +1,21 @@
 package org.d3if3137.assessment1.util
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.d3if3137.assessment1.database.KalkulatorDao
+import org.d3if3137.assessment1.ui.screen.DetailViewModel
+import org.d3if3137.assessment1.ui.screen.MainViewModel
+
+class ViewModelFactory(
+    private val dao: KalkulatorDao
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
